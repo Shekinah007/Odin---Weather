e5b975329db7b4a96753425d178d3363e05ea121
Shekinah <shalomsheks@gmail.com>
2022-10-13 22:11:58 +0100

    Move component.js code  to script.js

--- script.js
+++ script.js
@@ -1,4 +1,38 @@
-import populateWeatherInfo from "./components.js";
+// import populateWeatherInfo from "./components.js";
+
+const main = document.querySelector(".main");
+const description = document.querySelector(".description");
+const icon = document.querySelector(".icon");
+const wind = document.querySelector(".wind");
+const humidity = document.querySelector(".humidity");
+const pressure = document.querySelector(".pressure");
+const visibility = document.querySelector(".visibility");
+const feelsLike = document.querySelector(".feels_like");
+const temp = document.querySelector(".temp");
+const city = document.querySelector(".city");
+
+let array = [
+  main,
+  description,
+  icon,
+  wind,
+  humidity,
+  pressure,
+  visibility,
+  feelsLike,
+  temp,
+];
+
+function populateWeatherInfo(weather) {
+  weatherInfo.classList.add("show");
+  city.innerText = weather.city;
+  array.forEach((item) => {
+    console.log(item);
+    if (item.className == "icon") {
+      item.src = `http://openweathermap.org/img/wn/${weather.icon}@2x.png`;
+    } else item.innerText = weather[item.className];
+  });
+}
 
 const body = document.querySelector("body");
 const cityP = document.querySelector(".cityP");
